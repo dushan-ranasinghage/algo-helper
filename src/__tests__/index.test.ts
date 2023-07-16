ecf594b00a732ba899b38fcc362d9ab42ca648a4
Dushan Ranasinghage <dushanrandika1994@gmail.com>
2023-07-16 22:28:22 +1200

    Added search algorithm tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,6 @@
 import _algo from '../';
 
-describe('Sorting Tests', () => {
+describe('Sorting Test Suite', () => {
   const arr: number[] = [4, 2, 1, 5, 3, 7, 6];
   const sortedResult: number[] = [1, 2, 3, 4, 5, 6, 7];
 
@@ -32,5 +32,33 @@ describe('Sorting Tests', () => {
   test('Sorting array using heap sort', () => {
     const sortedArr: number[] = _algo.heapSort(arr);
     expect(sortedArr).toEqual(sortedResult);
+  });
+});
+
+describe('Searching Test Suite', () => {
+  const arr: number[] = [4, 2, 1, 5, 3, 7, 6];
+  const targetNumberSuccess = 5;
+  const targetNumberSuccessIndex = 3;
+  const targetNumberFail = 9;
+  const targetNumberFailIndex = -1;
+
+  test('Linear search when number is there', () => {
+    const resultIndex: number = _algo.linearSearch(arr, targetNumberSuccess);
+    expect(resultIndex).toBe(targetNumberSuccessIndex);
+  });
+
+  test('Linear search when number is not there', () => {
+    const resultIndex: number = _algo.linearSearch(arr, targetNumberFail);
+    expect(resultIndex).toBe(targetNumberFailIndex);
+  });
+
+  test('Binary search when number is there', () => {
+    const resultIndex: number = _algo.binarySearch(arr, targetNumberSuccess);
+    expect(resultIndex).toBe(targetNumberSuccessIndex);
+  });
+
+  test('Binary search when number is not there', () => {
+    const resultIndex: number = _algo.binarySearch(arr, targetNumberFail);
+    expect(resultIndex).toBe(targetNumberFailIndex);
   });
 });
